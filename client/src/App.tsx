7344b8034b97b76e0c5a29b330b6437cd2603a4a
Higor Soares <higor.15nwa@gmail.com>
2023-11-09 18:35:31 -0300

    Third commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Signin from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import Header from "./Components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* Header */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<NotFound />} />
