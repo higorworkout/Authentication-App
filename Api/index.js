31d6854678a9095d9e3f51f26c2a8dd699d7be88
Higor Soares <higor.15nwa@gmail.com>
2023-11-10 17:05:05 -0300

    Mongo db connection

--- Api/index.js
+++ Api/index.js
@@ -1,5 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv'
 const app = express();
+dotenv.config();
+
+//Conection with mongo
+import mongoose from 'mongoose';
+
+ mongoose.connect(process.env.MONGO, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((e) => {
+    console.log(e);
+})
+
+
 
 
 app.get("/", (req, res) => {
