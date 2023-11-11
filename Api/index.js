37ac947412193b97d2ead79b2618e87faf559f0f
Higor Soares <higor.15nwa@gmail.com>
2023-11-11 18:42:27 -0300

    Create auth router

--- Api/index.js
+++ Api/index.js
@@ -31,3 +31,15 @@ app.listen(3000, () => {
     console.log("Server on port 3000!");
 })
 
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+
+    return res.status(statusCode).json({
+        success: false, 
+        message,
+        statusCode: statusCode
+    })
+})
+
